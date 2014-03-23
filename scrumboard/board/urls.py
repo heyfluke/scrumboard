e8e8bd7cf30d5f08029043a9cbb8894512fd8433
Luke <fluke.l@gmail.com>
2014-03-24 01:32:00 +0800

    add login page and set login to required

--- scrumboard/board/urls.py
+++ scrumboard/board/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
 from scrumboard.board.api import StoryResource, StageResource, BoardResource
+from django.contrib.auth import views as auth_views
 
 api = Api(api_name='api')
 api.register(StoryResource())
@@ -10,4 +11,7 @@ api.register(BoardResource())
 urlpatterns = patterns('',
     url(r'^$', 'scrumboard.board.views.app', name='dashboard'),
     (r'^', include(api.urls)),
+    
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
+
--- scrumboard/board/views.py
+++ scrumboard/board/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render_to_response
 #from django.template.context import RequestContext
 #from scrumboard.core.decorators import render_template
 from scrumboard.settings import STATIC_URL
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def app(request):
     return render_to_response("board/app.html",{"STATIC_URL":STATIC_URL})
 
+@login_required
 def home(request):
     return render_to_response("index.html",{"STATIC_URL":STATIC_URL})
